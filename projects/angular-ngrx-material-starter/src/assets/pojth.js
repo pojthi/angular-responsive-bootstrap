f8d23019395951b6815222f7fc12dbade3f2b59f
poj <pojthi@gmail.com>
2023-06-09 04:06:56 +0700

    1

---
+++ projects/angular-ngrx-material-starter/src/assets/pojth.js
@@ -0,0 +1,13 @@
+function test(){
+    alert("Hello Poj!!!");
+}
+
+function changeinput1(){
+    document.getElementById("input1").style.display = 'block';
+    document.getElementById("input2").style.display = 'none';
+}
+
+function changeinput2(){
+    document.getElementById("input1").style.display = 'none';
+    document.getElementById("input2").style.display = 'block';
+}
